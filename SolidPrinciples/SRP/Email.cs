7d8e8cbf1cc3c3b315482c354893fc8dc9b0910d
ganeshraut302 <ganeshraut302@gmail.com>
2020-11-01 08:24:17 -0800

    Added SRP classes

---
+++ SolidPrinciples/SRP/Email.cs
@@ -0,0 +1,18 @@
+﻿namespace SolidPrinciples
+{
+    /// <summary>
+    /// Class to handle mail related functionality
+    /// </summary>
+    public class Email
+    {
+        public void CreateMailContent()
+        {
+            /// code to generate mail content
+        }
+
+        public void SendMail()
+        {
+            /// code to send mail
+        }
+    }
+}
---
+++ SolidPrinciples/SRP/Survey.cs
@@ -0,0 +1,18 @@
+﻿namespace SolidPrinciples
+{
+    /// <summary>
+    /// Survey class represents survey related activities
+    /// </summary>
+    public class Survey
+    {
+        public void CreateSurvey()
+        {
+            /// code to create the survey
+        }
+
+        public void EditSurvey()
+        {
+            /// code to edit survey
+        }
+    }
+}
